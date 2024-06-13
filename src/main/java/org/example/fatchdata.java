5cd1f1be931fd4b629215dd7cc0978c55f8c30ef
Rehaan Ahmad <rehaansufyaan1@gmail.com>
2024-06-13 16:06:30 +0530

    delelect

--- src/main/java/org/example/fatchdata.java
+++ src/main/java/org/example/fatchdata.java
@@ -3,7 +3,7 @@ package org.example;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static org.example.jdbcsite.scanner;
+import static org.example.jdbcsite.*;
 
 public class fatchdata {
     int id;
@@ -12,22 +12,29 @@ public class fatchdata {
     int phone;
     public static void display(ResultSet resultSet) throws SQLException {
         while (resultSet.next()){
-            System.out.print("id "+resultSet.getInt("id")+" ");
+            System.out.print("id "+resultSet.getInt("id")+ " ");
             System.out.print("name "+resultSet.getNString("name")+" ");
             System.out.print("email "+resultSet.getNString("email")+" ");
             System.out.print("phone "+resultSet.getNString("phone")+" ");
             System.out.println();
         }
     }
-    public  void insert(ResultSet resultSet){
-        System.out.println("insert id num");
-        id=scanner.nextInt();
+    public  void insert(){
+
         System.out.println("insert name");
         name=scanner.next();
         System.out.println("insert email");
          email=scanner.next();
         System.out.println("insert phone num");
         phone =scanner.nextInt();
-
     }
-}
+    public static void del(int key) throws SQLException {
+//        int suc = statement.executeUpdate("delete from emp where id=" + "+key+");
+        int suc = statement.executeUpdate("delete from emp where id=" + key);
+
+        if (suc > 0) {
+            System.out.println("delect sucess full");
+        } else {
+            System.out.println("not sslect");
+        }
+    }}
--- src/main/java/org/example/jdbcsite.java
+++ src/main/java/org/example/jdbcsite.java
@@ -13,42 +13,51 @@ public class jdbcsite {
         String password = "9336119497";
         try {
 //            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection= DriverManager.getConnection(url,user,password);
-            statement=connection.createStatement();
+            Connection connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
 
 
-        System.out.println("display the data press 1");
-        System.out.println("insert the data press 2");
+            System.out.println("display the data press 1");
+            System.out.println("insert the data press 2");
+            System.out.println("if you want to del the data ,plese inter the id");
+            int val = scanner.nextInt();
+            switch (val) {
+                case 1:
+                    resultSet = statement.executeQuery("select*from emp");
+                    fatchdata.display(resultSet);
+                    break;
+                case 2:
+                    resultSet = statement.executeQuery("select max(id) as nummax from emp");
 
-        int val=scanner.nextInt();
-        switch (val){
-            case 1:
-                resultSet=statement.executeQuery("select*from emp");
-               fatchdata.display(resultSet);
-               break;
-            case 2:
-                resultSet=statement.executeQuery("select max(id) as nummax from emp");
-                boolean max=resultSet.next();
-                while (max){
-                    int max_id=resultSet.getInt("nummax");
-                    System.out.println(max_id);
-                }
-                
-                fatchdata store=new fatchdata();
-                int col=statement.executeUpdate("insert into emp(id,name,email,phone) values("+store.id+",'+store.name+','+store.email+','+store.phone+')");
-      if(col>0){
-                System.out.println("insert the data ");
-            }else {
-                System.out.println("not insert");
+                    int max_id = 0;
+                    while (resultSet.next()) {
+                        max_id = resultSet.getInt("nummax");
+                        System.out.println(max_id);
+
+                    }
+                    max_id++;
+
+                    fatchdata store = new fatchdata();
+                    store.insert();
+                    int col = statement.executeUpdate("insert into emp(id,name,email,phone) values( " + max_id + " ,'" + store.name + "','" + store.email + "','" + store.phone + "')");
+                    if (col > 0) {
+                        System.out.println("insert the data ");
+                    } else {
+                        System.out.println("not insert");
+                    }
+                    break;
+                case 3:
+                    System.out.println("which value are you delect");
+                    int key = scanner.nextInt();
+                    fatchdata.del(key);
+                    break;
             }
-      break;
-            case 3:
-            break;
-            default:
-                System.out.println("enter the valid number");
-        }
-    }catch (Exception  e){
-        e.printStackTrace();
-    }
+
+
+            }
+        catch(Exception e){
+                e.printStackTrace();
+            }
+
     }
 }
